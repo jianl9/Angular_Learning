c51f9ac797baf995ae936fb3a5846ab675540e1e
Jian Li <jianl9@ics.uci.edu>
2019-07-02 02:40:31 -0700

    header&footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,16 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import {MatListModule} from '@angular/material';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
