b4ca11ef601994d15d48015a6fa51d5629d9aa0a
Jian Li <jianl9@ics.uci.edu>
2019-07-07 16:36:51 -0700

    RxJS 1.1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Leader} from '../shared/leader';
 import {LEADERS} from '../shared/leaders';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +12,14 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS), 1000);
-    });
+    return of(LEADERS).pipe(delay(500)).toPromise();
   }
 
   getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 1000);
-    });
+    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(500)).toPromise();
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return  new Promise(resolve => {
-      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 1000);
-    });
+    return of(LEADERS.filter((dish) => dish.featured)[0]).pipe(delay(500)).toPromise();
   }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import {DISHES} from '../shared/dishes';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +14,15 @@ export class PromotionService {
   constructor() {}
 
   getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS), 1000);
-    });
+    return of(PROMOTIONS).pipe(delay(500)).toPromise();
   }
 
   getPromotion(id: string): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 1000);
-    });
+    return of(PROMOTIONS.filter((promotion) => (promotion.id === id)))[0].pipe(delay(500)).toPromise();
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(Promise.resolve(PROMOTIONS.filter((promo) => promo.featured)[0])), 1000);
-    });
+    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(500)).toPromise();
   }
 }
 
