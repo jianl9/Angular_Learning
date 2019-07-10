4ba03aefa0724a5be9aa1c6a25f5767d1e51e937
Jian Li <jianl9@ics.uci.edu>
2019-07-10 02:32:05 -0700

    directives

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Renderer2, HostListener  } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]' // wherever I want to use this directive, I will use an attribute called appHighlight whith that particular element in my template
+})
+export class HighlightDirective {
+
+  constructor(private el: ElementRef,
+              private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') onMouseEnter() { // onmousehover highlight
+    this.renderer.addClass(this.el.nativeElement, 'highlight');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.renderer.removeClass(this.el.nativeElement, 'highlight');
+  }
+
+}
